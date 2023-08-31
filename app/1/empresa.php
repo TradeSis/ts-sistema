6e9d038a22faa5419b845366052d859194ee8ba8
Lucas Rosa <lucas.rosa@tradesis.com.br>
2023-08-31 13:14:46 -0300

    adicionado log
    - adicionado log em:
    aplicativo, empresa, login e loginaplicativo

--- app/1/empresa.php
+++ app/1/empresa.php
@@ -2,6 +2,29 @@
 // helio 31012023 - ajustado a api para receber o jsonEntrada, e pegar parametro od idCliente
 // helio 26012023 18:10 - Criacao primeira api - falta parametros para where
 //echo "-ENTRADA->".json_encode($jsonEntrada)."\n";
+
+//LOG
+$LOG_CAMINHO=defineCaminhoLog();
+if (isset($LOG_CAMINHO)) {
+    $LOG_NIVEL=defineNivelLog();
+    $identificacao=date("dmYHis")."-PID".getmypid()."-"."empresa";
+    if(isset($LOG_NIVEL)) {
+        if ($LOG_NIVEL>=1) {
+            $arquivo = fopen(defineCaminhoLog()."services_".date("dmY").".log","a");
+        }
+    }
+    
+}
+if(isset($LOG_NIVEL)) {
+    if ($LOG_NIVEL==1) {
+        fwrite($arquivo,$identificacao."\n");
+    }
+    if ($LOG_NIVEL>=2) {
+        fwrite($arquivo,$identificacao."-ENTRADA->".json_encode($jsonEntrada)."\n");
+    }
+}
+//LOG
+
 $idEmpresa = null;
 
 $conexao = conectaMysql($idEmpresa);
@@ -13,6 +36,15 @@ if (isset($jsonEntrada["idEmpresa"])) {
 }
 
 //echo $sql;
+
+//LOG
+if(isset($LOG_NIVEL)) {
+  if ($LOG_NIVEL>=3) {
+      fwrite($arquivo,$identificacao."-SQL->".$sql."\n");
+  }
+}
+//LOG
+
 $rows = 0;
 $buscar = mysqli_query($conexao, $sql);
 while ($row = mysqli_fetch_array($buscar, MYSQLI_ASSOC)) {
@@ -26,5 +58,11 @@ if (isset($jsonEntrada["idEmpresa"]) && $rows==1) {
 $jsonSaida = $empresa;
 
 
-
+//LOG
+if(isset($LOG_NIVEL)) {
+  if ($LOG_NIVEL>=2) {
+      fwrite($arquivo,$identificacao."-SAIDA->".json_encode($jsonSaida)."\n\n");
+  }
+}
+//LOG
 ?>
--- app/1/login_alterar.php
+++ app/1/login_alterar.php
@@ -1,6 +1,26 @@
 <?php
- //echo "-ENTRADA->".json_encode($jsonEntrada)."\n";
+//echo "-ENTRADA->".json_encode($jsonEntrada)."\n";
 
+//LOG
+$LOG_CAMINHO = defineCaminhoLog();
+if (isset($LOG_CAMINHO)) {
+    $LOG_NIVEL = defineNivelLog();
+    $identificacao = date("dmYHis") . "-PID" . getmypid() . "-" . "login_alterar";
+    if (isset($LOG_NIVEL)) {
+        if ($LOG_NIVEL >= 1) {
+            $arquivo = fopen(defineCaminhoLog() . "services_" . date("dmY") . ".log", "a");
+        }
+    }
+}
+if (isset($LOG_NIVEL)) {
+    if ($LOG_NIVEL == 1) {
+        fwrite($arquivo, $identificacao . "\n");
+    }
+    if ($LOG_NIVEL >= 2) {
+        fwrite($arquivo, $identificacao . "-ENTRADA->" . json_encode($jsonEntrada) . "\n");
+    }
+}
+//LOG
 
 $conexao = conectaMysql();
 if (isset($jsonEntrada['idLogin'])) {
@@ -9,32 +29,59 @@ if (isset($jsonEntrada['idLogin'])) {
     $email = $jsonEntrada['email'];
     $cpfCnpj = $jsonEntrada['cpfCnpj'];
     $pedeToken = $jsonEntrada['pedeToken'];
-    
+
     $sql = "UPDATE `login` SET `loginNome`='$loginNome', `email`='$email', `cpfCnpj`='$cpfCnpj', `pedeToken`=$pedeToken WHERE idLogin = $idLogin";
-    
+
     if (isset($jsonEntrada['password'])) {
-    $password = md5($jsonEntrada['password']);
-    $sql = "UPDATE `login` SET `password` = '$password' WHERE idLogin = $idLogin";
+        $password = md5($jsonEntrada['password']);
+        $sql = "UPDATE `login` SET `password` = '$password' WHERE idLogin = $idLogin";
     }
     //echo "-ENTRADA->".$sql."\n"; 
-    
-    if ($atualizar = mysqli_query($conexao, $sql)) {
+
+    //LOG
+    if (isset($LOG_NIVEL)) {
+        if ($LOG_NIVEL >= 3) {
+            fwrite($arquivo, $identificacao . "-SQL->" . $sql . "\n");
+        }
+    }
+    //LOG
+
+    //TRY-CATCH
+    try {
+
+        $atualizar = mysqli_query($conexao, $sql);
+        if (!$atualizar)
+            throw new Exception(mysqli_error($conexao));
+
         $jsonSaida = array(
             "status" => 200,
             "retorno" => "ok"
         );
-    } else {
+    } catch (Exception $e) {
         $jsonSaida = array(
             "status" => 500,
-            "retorno" => "erro no mysql"
+            "retorno" => $e->getMessage()
         );
+        if ($LOG_NIVEL >= 1) {
+            fwrite($arquivo, $identificacao . "-ERRO->" . $e->getMessage() . "\n");
+        }
+    } finally {
+        // ACAO EM CASO DE ERRO (CATCH), que mesmo assim precise
     }
+    //TRY-CATCH
+
+
 } else {
     $jsonSaida = array(
         "status" => 400,
         "retorno" => "Faltaram parametros"
     );
-
 }
 
-?>
+//LOG
+if (isset($LOG_NIVEL)) {
+    if ($LOG_NIVEL >= 2) {
+        fwrite($arquivo, $identificacao . "-SAIDA->" . json_encode($jsonSaida) . "\n\n");
+    }
+}
+//LOG
