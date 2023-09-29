4a981f97619a3dd74fcde14f178571bb2fbe0d60
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-09-29 15:18:22 -0300

    novo modelo de painel

---
+++ novoindex.php
@@ -0,0 +1,180 @@
+<?php
+include_once __DIR__ . "/../config.php";
+
+
+include_once "head.php";
+include_once ROOT . "/sistema/database/loginAplicativo.php";
+$nivelMenuLogin = null;
+
+if ($_SESSION["idEmpresa"] == 1) { // Proteção
+    $nivelMenuLogin = buscaLoginAplicativo($_SESSION['idLogin'], 'Sistema'); //Sistema
+}
+
+$configuracao = 1;
+$nivelMenu = 0;
+
+if ($nivelMenuLogin == null) {
+    return;
+} else {
+    $nivelMenu = $nivelMenuLogin['nivelMenu'];
+}
+
+
+?>
+
+
+<div class="container-fluid mt-1" style="background-color: #EEEEEE;">
+    <div class="row border-bottom">
+        <div class="col-md-12 d-flex justify-content-center">
+            <ul class="nav a" id="myTabs">
+
+
+                <?php
+
+                           
+                $tab = '';
+
+                if (isset($_GET['tab'])) {
+                    $tab = $_GET['tab'];
+                }
+
+                ?>
+
+
+                <?php if ($nivelMenu == 5) {
+                    if ($tab == '') {
+                        $tab = 'empresa';
+                    } ?>
+                    <li class="nav-item mr-1 ">
+                        <a class="nav-link1 nav-link <?php if ($tab == "empresa") {
+                            echo " active ";
+                        } ?>" href="?tab=empresa"
+                            role="tab">Empresa</a>
+                    </li>
+                <?php }
+                if ($nivelMenu == 5) { ?>
+                    <li class="nav-item mr-1 ">
+                        <a class="nav-link1 nav-link <?php if ($tab == "login") {
+                            echo " active ";
+                        } ?>" href="?tab=login"
+                            role="tab">Login</a>
+                    </li>
+                <?php }
+                if ($nivelMenu == 5) { ?>
+                    <li class="nav-item mr-1 ">
+                        <a class="nav-link1 nav-link <?php if ($tab == "aplicativo") {
+                            echo " active ";
+                        } ?>"
+                            href="?tab=aplicativo" role="tab">Aplicativos</a>
+                    </li>
+                <?php }
+             if ($nivelMenu==5) { ?>
+                <li class="nav-item mr-1 ">
+                    <a class="nav-link1 nav-link <?php if ($tab=="anexos") {echo " active ";} ?>" 
+                        href="?tab=anexos" 
+                        role="tab"                        
+                        >Anexos</a>
+                </li>
+            <?php } ?>
+
+
+            </ul>
+
+
+        </div>
+
+    </div>
+
+</div>
+
+<?php
+
+$src = "";
+$title = "Sistema";
+
+if ($tab == "empresa") {
+    $src = "configuracao/empresa.php";
+    $title = "Sistema/Empresa";
+
+}
+if ($tab == "login") {
+    $src = "configuracao/login.php";
+    $title = "Sistema/Login";
+}
+if ($tab == "aplicativo") {
+    $src = "configuracao/aplicativo.php";
+    $title = "Sistema/Aplicativo";
+}
+if ($tab == "anexos") {
+    $src = "configuracao/anexos.php";
+    $title = "Sistema/Anexos";
+}
+      
+if ($tab == "configuracao") {
+    $src = "configuracao/";
+    $title = "Sistema/Configuração";
+    if (isset($_GET['stab'])) {
+        $src = $src . "?stab=" . $_GET['stab'];
+
+
+    }
+
+
+}
+
+if ($src !== "") {
+    //echo URLROOT ."/sistema/". $src;
+    ?>
+
+
+        <div class="container-fluid p-0 m-0">
+            <iframe class="row p-0 m-0" id="iFrameTab" style="width: 100%; height: 92vh; border:none"
+                src="<?php echo URLROOT ?>/sistema/<?php echo $src ?>"></iframe>
+        </div>
+        
+        <?php
+}
+?>
+
+<script>
+        var tab;
+        var tabContent;
+
+        window.onload = function () {
+            tabContent = document.getElementsByClassName('tabContent');
+            tab = document.getElementsByClassName('tab');
+            hideTabsContent(1);
+        }
+
+        document.getElementById('tabs').onclick = function (event) {
+            var target = event.target;
+            if (target.className == 'tab') {
+                for (var i = 0; i < tab.length; i++) {
+                    if (target == tab[i]) {
+                        showTabsContent(i);
+                        break;
+                    }
+                }
+            }
+        }
+
+        function hideTabsContent(a) {
+            for (var i = a; i < tabContent.length; i++) {
+                tabContent[i].classList.remove('show');
+                tabContent[i].classList.add("hide");
+                tab[i].classList.remove('whiteborder');
+            }
+        }
+
+        function showTabsContent(b) {
+            if (tabContent[b].classList.contains('hide')) {
+                hideTabsContent(0);
+                tab[b].classList.add('whiteborder');
+                tabContent[b].classList.remove('hide');
+                tabContent[b].classList.add('show');
+            }
+        }
+    </script>
+</body>
+
+</html>
--- novopainel.php
+++ novopainel.php
@@ -2,173 +2,151 @@
 <html lang="pt-BR">
 
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link rel="icon" href="../img/meucontrole_icon.png" type="image/png">
+    <title>meucontrole.pro</title>
 </head>
 
-<!-- head>
-    <title> Vertical Menu </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-</head -->
+
+<style>
+    .nav-item .nav-link {
+        font-weight: 600;
+        color: #ffffff;
+    }
+
+    .sidebar {
+        height: 100vh;
+        width: 150px;
+        background: #ccc;
+        transition: all 0.5s ease-in-out;
+    }
+
+    .itemsiderbar {
+        background-color: #13216A;
+        color: #ffffff;
+        text-decoration: none;
+    }
+
+    .itemsiderbar:hover,
+    .itemsiderbar:focus {
+        color: #ffffff;
+        /* background-color: #d2f4ea; */
+        border-bottom: 2px solid #d2f4ea;
+        font-weight: 900;
+        width: 100px;
+    }
+</style>
 
 <body>
-    <div class="container-fluid ">
+    <nav class="navbar-dark d-md-none p-2" style="background-color: #13216A;">
+        <div class="container-fluid">
 
-        <div class="row d-md-flex ">
-            <div class="p-0 col-md-1">
-                <!-- ul class="nav flex-md-column">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <p>LOGO</p>
-                        </a>
-                        
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Serviços</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Notas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Financeiro</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="#" >Sistema</a>
-                    </li>
-                </ul -->
-
-                <nav class="p-0 navbar navbar-light">
-                    <!-- logo em dispositivo grande -->
-                    <a class="navbar-brand d-none d-md-block" href="#"><img src="../img/logo.png" width="100%"></a>
-                    <!-- logo em dispositivo pequeno com botão -->
-                    <a class="navbar-brand  d-md-none" href="#"><img src="../img/logo.png" width="40%"></a>
-                    <button class="navbar-toggler d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="row d-flex flex">
+                <div class="col-3 col-sm-3 ">
+                    <a class="navbar-brand" href="#"><img src="../img/meucontrole.png" width="120vh 120vw"></a>
+                </div>
+                <div class="col-9 col-sm-9 text-end d-sm-none">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <!--   <div class="d-sm-block d-md-none " id="colp">
-                        <ul class="nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"> Home TABLET </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"> Services </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"> Sistema </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"> Blogs </a>
-                            </li>
-                        </ul>
-                    </div> -->
-
-                    <!-- menu dispositivo pequeno -->
-                    <div class="collapse d-md-none" id="navbarHeader">
-                        <ul class="nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"> Home TABLET </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"> Services </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"> Sistema </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#"> Blogs </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- menu dispositivo grande -->
-                    <div class="navbar-collapse collapse   d-none d-md-block">
-                        <ul class="nav navbar-nav" id="menu">
-                            <li class="nav-item">
-                                <a class="nav-link" src="" href="#"> Home NORMAL </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" src="../services/demandas/" href="#"> Services </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" src="configuracao/empresa.php" href="#"> Sistema </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" src="indexblog.php" href="#"> Blogs </a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-            <div class="col-md-11 bg-success" style="height: 100vh;">
-                <div class="row d-flex flex-row flex-sm-row-reverse">
-                    <div class="col-md-2">
-                        <div class="row">
-                            <div class="col-12 bg-danger">
-                                <h5 style="text-align:right">PERFIL</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-10">
-                        <ul class="nav nav-tabs ">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Agenda</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Execução</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Demandas</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Contratos</a>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
-                <div class="row" style="width: 100%; height: 90vh;">
+                <!-- MENU MOBILE FIXO -->
+                <div class="col-sm-12 d-none d-sm-block">
+                    <ul class="col-sm-12 nav text-center" id="menu">
+                        <li class="nav-item mt-1 col-2 ">
+                            <a class="nav-link active " aria-current="page" href="#" src="configuracao/empresa.php"> Serviços2</a>
+                        </li>
+                        <li class="nav-item mt-1 col-2 ">
+                            <a class="nav-link" href="#" src="configuracao/login.php"> Notas</a>
+                        </li>
+                        <li class="nav-item mt-1 col-2 ">
+                            <a class="nav-link" href="#" src="configuracao/aplicativo.php"> Financeiro</a>
+                        </li>
+                        <li class="nav-item mt-1 col-2 ">
+                            <a class="nav-link" href="#" src="configuracao/anexos.php"> Cadastros</a>
+                        </li>
+                        <li class="nav-item mt-1 col-2 ">
+                            <a class="nav-link" href="#" src="configuracao/anexos.php"> Paginas</a>
+                        </li>
+                        <li class="nav-item mt-1 col-2 ">
+                            <a class="nav-link" href="#" src="configuracao/anexos.php"> Sistema</a>
+                        </li>
 
-                    <iframe class="container-fluid" id="myIframe" src="iframe.php"></iframe>
-
+                    </ul>
                 </div>
             </div>
+            <!-- MENU MOBILE FIXO ATIVADO POR BOTÂO-->
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="nav mt-3 text-center" id="menu">
+                    <li class="nav-item col-sm-2">
+                        <a class="nav-link active" aria-current="page" href="#" src="configuracao/empresa.php"> Serviços1</a>
+                    </li>
+                    <li class="nav-item col-sm-2">
+                        <a class="nav-link" href="#" src="configuracao/login.php"> Notas</a>
+                    </li>
+                    <li class="nav-item col-sm-2">
+                        <a class="nav-link" href="#" src="configuracao/aplicativo.php"> Financeiro</a>
+                    </li>
+                    <li class="nav-item col-sm-2">
+                        <a class="nav-link" href="#" src="configuracao/anexos.php"> Cadastros</a>
+                    </li>
+                    <li class="nav-item col-sm-2">
+                        <a class="nav-link" href="#" src="configuracao/anexos.php"> Paginas</a>
+                    </li>
+                    <li class="nav-item col-sm-2">
+                        <a class="nav-link" href="#" src="configuracao/anexos.php"> Sistema</a>
+                    </li>
 
+                </ul>
+            </div>
         </div>
-    </div>
+    </nav>
+
+    <div class="d-flex" style="background-color: #EEEEEE;">
+        <div class="sidebar pt-2 d-none d-md-block" style="background-color: #13216A;">
+            <a class="p-3" href="#"><img src="../img/meucontrole.png" width="120vh 120vw"></a>
+            <div class="list-group mt-4" id="menu">
+                <a class="itemsiderbar   p-3" href="#" src="../services/">Serviços</a>
+                <a class="itemsiderbar   p-3" href="#" src="#!">Notas</a>
+                <a class="itemsiderbar   p-3" href="#" src="#">Financeiro</a>
+                <a class="itemsiderbar   p-3" href="#" src="#">Cadastros</a>
+                <a class="itemsiderbar   p-3" href="#" src="#">Paginas</a>
+                <a class="itemsiderbar   p-3" href="#" src="novoindex.php">Sistema</a>
+            </div>
+        </div>
+        <div class="container-fluid p-0 m-0">
+            <div class="row p-0 m-0" style="width: 100%; height: 100vh;">
+                <iframe class="container-fluid" id="myIframe" src="iframe.php"></iframe>
+            </div>
+        </div>
+
     </div>
 
+
+    <!-- JQUERY TEMPORARIO -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script type="text/javascript">
+        $(document).ready(function() {
 
 
-$(document).ready(function () {
+            // SELECT MENU
+            $("#menu a").click(function() {
+
+                var value = $(this).text();
+                value = $(this).attr('src');
+
+                //IFRAME TAG
+                if (value != '') {
+                    $("#myIframe").attr('src', value);
+                }
+
+            })
 
 
-    // SELECT MENU
-    $("#menu a").click(function () {
-
-        var value = $(this).text();
-        value = $(this).attr('src');
-
-        //IFRAME TAG
-        if (value != '') {
-            $("#myIframe").attr('src', value);
-        }
-
-    })
-
-
-});
-
-
-</script>
+        });
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
