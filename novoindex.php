a5b94d30654894e8b0d176e8b59ee8fc03a83851
Lucas Rosa <100045049+Lucas-TradeSis@users.noreply.github.com>
2023-10-05 17:03:28 -0300

    ajuste de head do programa novo index.php
    - removido alguns link, pois estarão no vendor

--- novoindex.php
+++ novoindex.php
@@ -21,16 +21,8 @@ if ($nivelMenuLogin == null) {
 <!doctype html>
 <html lang="pt-BR">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <?php include_once ROOT. "/vendor/head_css.php";?>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <link href="<?php echo URLROOT ?>/sistema/css/painel.css" rel="stylesheet" type="text/css">
     
-    <link rel="icon" href="../img/meucontrole_icon.png" type="image/png">
-
+    <?php include_once ROOT. "/vendor/head_css.php";?>
     <title>Sistema</title>
 
 </head>
