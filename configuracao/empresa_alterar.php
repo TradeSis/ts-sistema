aefa4d7bf3527e96ef8ec04ee9387aa6d78e71f1
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2023-11-08 09:58:55 -0300

    pessoas/empresa

--- configuracao/empresa_alterar.php
+++ configuracao/empresa_alterar.php
@@ -5,7 +5,9 @@
 
 include_once('../header.php');
 include_once('../database/empresa.php');
+include_once(ROOT . '/cadastros/database/pessoas.php');
 $empresa = buscaEmpresas($_GET['idEmpresa']);
+$pessoas = buscarPessoa();
 ?>
 <!doctype html>
 <html lang="pt-BR">
@@ -13,6 +15,7 @@ $empresa = buscaEmpresas($_GET['idEmpresa']);
 <head>
 
     <?php include_once ROOT . "/vendor/head_css.php"; ?>
+    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
 
 </head>
 
@@ -51,6 +54,32 @@ $empresa = buscaEmpresas($_GET['idEmpresa']);
                     <input type="number" min="1" value="<?php echo $empresa['timeSessao'] ?>" class="form-control ts-input" name="timeSessao" autocomplete="off" required>
                 </div>
             </div>
+            <div class="row mt-3">
+                <div class="col-md-3">
+                    <label class="form-label ts-label">Pessoa</label>
+                    <select class="form-select ts-input" name="idPessoa">
+                        <option value="<?php echo null ?>"></option>
+                        <?php
+                        foreach ($pessoas as $pessoa) {
+                            ?>
+                            <option <?php if ($pessoa['idPessoa']  == $empresa['idPessoa'] ) {
+                                    echo "selected";
+                                } ?> value="<?php echo $pessoa['idPessoa'] ?>">
+                                <?php echo $pessoa['nomePessoa'] ?>
+                            </option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class="col-md-9">
+                    <div class="container-fluid p-0">
+                        <div class="col">
+                            <span class="tituloEditor">Menu</span>
+                        </div>
+                        <div class="quill-menu" style="height:20vh !important"></div>
+                        <textarea style="display: none" id="quill-menu" name="menu"><?php echo $empresa['menu']; ?></textarea>
+                    </div>
+                </div>
+            </div>     
 
             <div class="text-end mt-4">
                 <button type="submit" class="btn  btn-success"><i class="bi bi-sd-card-fill"></i>&#32;Salvar</button>
@@ -62,6 +91,21 @@ $empresa = buscaEmpresas($_GET['idEmpresa']);
     <!-- LOCAL PARA COLOCAR OS JS -->
 
     <?php include_once ROOT . "/vendor/footer_js.php"; ?>
+    <!-- QUILL editor -->
+    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+    
+    <script>
+        var menu = new Quill('.quill-menu', {
+            theme: 'snow'
+        });
+
+        var initialContent = document.getElementById('quill-menu').value;
+        menu.clipboard.dangerouslyPasteHTML(0, initialContent);
+
+        menu.on('text-change', function(delta, oldDelta, source) {
+            $('#quill-menu').val(menu.container.firstChild.innerHTML);
+        });
+    </script>
 
     <!-- LOCAL PARA COLOCAR OS JS -FIM -->
 
