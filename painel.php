1688e22cd2cc5352c35bc6e0e49777d9034f5fd3
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2024-03-05 14:11:07 -0300

    Update painel.php

--- painel.php
+++ painel.php
@@ -48,6 +48,18 @@ $url = (parse_url($URL_ATUAL, PHP_URL_PATH));
             href="<?php echo URLROOT ?>/paginas/">Paginas</a>
         <?php }
         **/
+        if (in_array("Crediario", $aplicativo)) { ?>
+            <a class="ts-itemsiderbar <?php if ($url == URLROOT . "/crediario/") {echo " active ";} ?> p-3" 
+            href="<?php echo URLROOT ?>/crediario/">Crediario</a>
+        <?php }
+        if (in_array("Vendas", $aplicativo)) { ?>
+            <a class="ts-itemsiderbar <?php if ($url == URLROOT . "/vendas/") {echo " active ";} ?> p-3" 
+            href="<?php echo URLROOT ?>/vendas/">Vendas</a>
+        <?php }
+        if (in_array("Relatorios", $aplicativo)) { ?>
+            <a class="ts-itemsiderbar <?php if ($url == URLROOT . "/relatorios/") {echo " active ";} ?> p-3" 
+            href="<?php echo URLROOT ?>/relatorios/">Relatorios</a>
+        <?php }
         if (in_array("Impostos", $aplicativo)) { ?>
             <a class="ts-itemsiderbar <?php if ($url == URLROOT . "/impostos/") {echo " active ";} ?> p-3" 
             href="<?php echo URLROOT ?>/impostos/">Impostos</a>
